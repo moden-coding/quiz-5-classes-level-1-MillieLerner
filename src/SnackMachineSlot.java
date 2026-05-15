dec69e28b54f4292792d56583526804064c83e58
MillieLerner <28mlerner@gannacademy.org>
2026-05-15 10:05:12 -0400

    Done

--- src/SnackMachineSlot.java
+++ src/SnackMachineSlot.java
@@ -1,3 +1,43 @@
 public class SnackMachineSlot {
-    
+
+    private String item;
+    private double price;
+    private int amount;
+    private int sold;
+    private double money;
+
+    public SnackMachineSlot(String i, double p, int a) {
+        item = i;
+        price = p;
+        if (price <= 0) {
+            price = 0.0;
+        }
+        amount = a;
+        if (amount <= 0) {
+            amount = 0;
+        }
+        money = 0;
+    }
+
+    public void purchaseOne() {
+        if (amount > 0) {
+            amount--;
+            sold++;
+        } else {
+            amount += 0;
+            sold += 0;
+        }
+        money+=price;
+    }
+
+    public double getRevenue() {
+        money=price*sold;
+        return money;
+    }
+
+    public String toString() {
+        return "Item: " + item + ", Price: " + price + ", In Stock: " + amount + ", Amount sold: " + sold + ", Total: "
+                + money;
+
+    }
 }
